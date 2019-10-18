f27b77fbf901f64bd981e5368e07d46f241dd2f7
Sergey Ryabenko <abure9992@gmail.com>
2019-10-18 21:00:23 +0300

    Updates

--- src/scripts/config.js
+++
@@ -1,7 +0,0 @@
-export const UserConfig = {
-  baseUrl: 'https://praktikum.tk/cohort2',
-  headers: {
-    authorization: '35b50ab1-abf3-4779-8fb6-3cb0eb33b8d6',
-    'Content-Type': 'application/json'
-  }
-}
